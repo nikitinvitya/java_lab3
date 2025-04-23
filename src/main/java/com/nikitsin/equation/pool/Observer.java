1e27947994488264810a3928716f2227bac3952b
nikitinvitya <149611289+nikitinvitya@users.noreply.github.com>
2025-04-23 14:27:27 +0300

    Delete src directory

--- src/main/java/com/nikitsin/equation/pool/Observer.java
+++
@@ -1,7 +0,0 @@
-package com.nikitsin.equation.pool;
-
-import com.nikitsin.equation.entity.QuadraticEquation;
-
-public interface Observer {
-    void update(QuadraticEquation equation);
-}
--- src/main/java/com/nikitsin/equation/validator/EquationValidator.java
+++
@@ -1,7 +0,0 @@
-package com.nikitsin.equation.validator;
-
-import com.nikitsin.equation.entity.QuadraticEquation;
-
-public interface EquationValidator {
-    boolean isValid(QuadraticEquation eq);
-}
