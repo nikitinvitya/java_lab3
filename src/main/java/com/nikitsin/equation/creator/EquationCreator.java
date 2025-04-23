a92625c652063320ac80eeb9a85c3dfa22239cec
nikitinvitya <149611289+nikitinvitya@users.noreply.github.com>
2025-04-23 14:25:56 +0300

    Delete src directory

--- src/main/java/com/nikitsin/equation/creator/EquationCreator.java
+++
@@ -1,7 +0,0 @@
-package com.nikitsin.equation.creator;
-import com.nikitsin.equation.entity.QuadraticEquation;
-import java.util.Optional;
-
-public interface EquationCreator {
-    Optional<QuadraticEquation> createEquation(double a, double b, double c);
-}
--- src/main/java/com/nikitsin/equation/entity/EquationState.java
+++
@@ -1,7 +0,0 @@
-package com.nikitsin.equation.entity;
-
-public enum EquationState {
-    NO_REAL_ROOTS,
-    ONE_REAL_ROOT,
-    TWO_REAL_ROOTS
-}
