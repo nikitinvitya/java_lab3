09d1756646813963bb65d998dba168230dc07ed5
nikitinvitya <149611289+nikitinvitya@users.noreply.github.com>
2025-04-23 13:56:04 +0300

    Delete src/main directory

--- src/main/java/com/nikitsin/equation/service/EquationSolver.java
+++
@@ -1,8 +0,0 @@
-package com.nikitsin.equation.service;
-
-import com.nikitsin.equation.entity.QuadraticEquation;
-import java.util.List;
-
-public interface EquationSolver {
-    List<Double> findRoots(QuadraticEquation eq);
-}
--- src/main/java/com/nikitsin/equation/specification/EquationSpecification.java
+++
@@ -1,7 +0,0 @@
-package com.nikitsin.equation.specification;
-
-import com.nikitsin.equation.entity.QuadraticEquation;
-
-public interface EquationSpecification {
-    boolean specified(QuadraticEquation equation);
-}
